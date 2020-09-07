360afca4b1389089c92b6c867556b0fdce534741
Bello S Babakolo <fleetbeekay@gmail.com>
2020-09-07 16:09:42 +0100

    Refactor my_each

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -1,12 +1,8 @@
 module Enumerable
   def my_each
-    return enum_for(:my_each) unless block_given?
-
-    for i in self
-      yield(i)
-    end
+    return enum_for(__method__) unless block_given?
+    
+    size.times { |idx| yield(to_a[idx]) }
     self
   end
 end
-
-p [1,2,3].my_each
