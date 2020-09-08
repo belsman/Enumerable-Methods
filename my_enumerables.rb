fce82b2af40204420830fc0d2d1a4f754c060c5a
RICKCOYL <rickoburu@gmail.com>
2020-09-08 10:55:07 +0300

    implement my_each_with_index

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -1,8 +1,20 @@
 module Enumerable
   def my_each
     return enum_for(__method__) unless block_given?
+
+    arr = to_a
+    size.times { |idx| yield(arr[idx]) }
+    self
+  end
+  
+  def my_each_with_index
+    return enum_for(__method__) unless block_given?
+
+    index = 
     
     size.times { |idx| yield(to_a[idx]) }
     self
   end
+
+
 end
