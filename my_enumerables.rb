13453b8e51981876c62a84ec50e8a9491d5b2df6
Bello S Babakolo <fleetbeekay@gmail.com>
2020-09-08 09:38:40 +0100

    Implement #my_select the naive way

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -15,5 +15,21 @@ module Enumerable
     self
   end
 
+  def my_select
+    return enum_for(__method__) unless block_given?
+    result = []
 
+    for el in self
+      boolean_value = yield(el) # true or false
+      result << el if boolean_value
+    end
+    result 
+  end
 end
+
+
+p (1..10).my_select { |i|  i % 3 == 0 }   #=> [3, 6, 9]
+
+p [1,2,3,4,5].my_select { |num|  num.even?  }   #=> [2, 4]
+
+p [:foo, :bar].my_select #{ |x| x == :foo }   #=> [:foo]
