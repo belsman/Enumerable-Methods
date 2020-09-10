8d0c778b82e52da8a0aad2dc981310f19278430f
Bello S Babakolo <fleetbeekay@gmail.com>
2020-09-10 09:50:48 +0100

    Fix edge cases for #my_all, #my_any, and my_none

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -23,30 +23,54 @@ module Enumerable
     result
   end
 
-  def my_all?
-    my_each do |el|
-      if block_given?
-        return false unless yield(el)
-      else
-        return false unless el
+  def my_all?(pattarn_or_cls=nil)
+    if pattarn_or_cls
+      my_each do |el|
+        if pattarn_or_cls.is_a?(Regexp)
+          return false unless el =~ pattarn_or_cls
+        elsif pattarn_or_cls.is_a?(Object)
+          return false unless el.is_a?(pattarn_or_cls)
+        end
+      end 
+    else
+      my_each do |el|
+        if block_given?
+          return false unless yield(el)
+        else
+          return false unless el
+        end
       end
     end
     true
   end
 
-  def my_any?
-    my_each do |el|
-      if block_given?
-        return true if yield(el)
-      elsif el
-        return true
+  def my_any?(pattarn_or_cls=nil)
+    if pattarn_or_cls
+      my_each do |el|
+        if pattarn_or_cls.is_a?(Regexp)
+          return true if el =~ pattarn_or_cls
+        elsif pattarn_or_cls.is_a?(Object)
+          return true if el.is_a?(pattarn_or_cls)
+        end
+      end 
+    else
+      my_each do |el|
+        if block_given?
+          return true if yield(el)
+        elsif el
+          return true
+        end
       end
     end
     false
   end
 
-  def my_none?
-    !my_any? { |el| yield(el) }
+  def my_none?(pattarn_or_cls=nil)
+    unless block_given?
+      !my_any?(pattarn_or_cls) { |el| el }
+    else
+      !my_any?(pattarn_or_cls) { |el| yield(el) }
+    end
   end
 
   def my_count(item = nil)
@@ -79,3 +103,12 @@ end
 def multiply_els(arr)
   arr.my_inject(1) { |product, n| product * n }
 end
+
+p %w{ant bear cat}.my_none? { |word| word.length == 5 } #=> true
+p %w{ant bear cat}.my_none? { |word| word.length >= 4 } #=> false
+p %w{ant bear cat}.my_none?(/d/)                        #=> true
+p [1, 3.14, 42].my_none?(Float)                         #=> false
+p [].my_none?                                           #=> true
+p [nil].my_none?                                        #=> true
+p [nil, false].my_none?                                 #=> true
+p [nil, false, true].my_none?                           #=> false
